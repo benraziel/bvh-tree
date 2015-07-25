25d9798a4bc047dd231833d0cb8c42c4c0b1912e
Ben Raziel <benraziel@gmail.com>
2015-07-25 15:18:30 +0300

    fixed tests

--- tests/bvh.spec.js
+++ tests/bvh.spec.js
@@ -1,13 +1,13 @@
 describe("bvhTests", function() {
     var twoTrianglesX = [
-        -5.0, 20.0, -30.0, -2.0, -50.0, 60.0, -1.0, 80.0, -90.0,
-        5.0, 21.0, -31.0, 2.0, -51.0, 61.0, 1.0, 81.0, -91.0
+        [{x: -5.0, y: 20.0, z: -30.0}, {x: -2.0, y: -50.0, z: 60.0}, {x:-1.0, y:80.0, z:-90.0}],
+        [{x: 5.0, y: 21.0, z: -31.0}, {x: 2.0, y: -51.0, z: 61.0}, {x:1.0, y:81.0, z:-91.0}]
     ];
 
     var threeTriangles = [
-        -10.0, 20.0, -30.0, 40.0, -50.0, 60.0, -70.0, 80.0, -90.0,
-        -11.0, 21.0, -31.0, 41.0, -51.0, 61.0, -71.0, 81.0, -91.0,
-        -12.0, 22.0, -32.0, 42.0, -55.0, 62.0, -78.0, 87.0, -92.0
+        [{x:-10.0, y: 20.0, z: -30.0}, {x: 40.0, y: -50.0, z:60.0}, {x: -70.0, y: 80.0, z:-90.0}],
+        [{x:-11.0, y: 21.0, z:-31.0}, {x: 41.0, y: -51.0, z:61.0}, {x: -71.0, y: 81.0, z:-91.0}],
+        [{x:-12.0, y: 22.0, z:-32.0}, {x: 42.0, y: -55.0, z:62.0}, {x: -78.0, y: 87.0, z:-92.0}]
     ];
 
     it("Should allow constructing an empty tree", function () {
@@ -18,9 +18,9 @@ describe("bvhTests", function() {
     });
 
     it("Should correctly calculate node extents box for a single triangle", function () {
-        var triangle = [-1.0, 2.0, -3.0, 4.0, -5.0, 6.0, -7.0, 8.0, -9.0];
+        var triangle = [{x: -1.0, y: 2.0, z: -3.0}, {x: 4.0, y: -5.0, z: 6.0}, {x: -7.0, y: 8.0, z: -9.0}];
 
-        var bvh = new BVH(triangle, 10);
+        var bvh = new BVH([triangle], 10);
         expect(bvh._rootNode._extentsMin).toEqual({'x': -7.0, 'y': -5.0, 'z': -9.0});
         expect(bvh._rootNode._extentsMax).toEqual({'x': 4.0, 'y': 8.0, 'z': 6.0});
     });
